8862b2a4eb04f30bb48af07976ea057535ac754e
kristykawai <kristy.kw.chan@gmail.com>
2024-05-10 17:54:24 +0930

    finished input output replacement

--- ex04/includes/Utils.hpp
+++ ex04/includes/Utils.hpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <fstream>
 
-void replace(std::string file, std::string findStr, std::string replaceWith);
+std::ifstream readFile(const std::string &file);
+std::ofstream createProcessedFile(std::ifstream &inFile, std::string &outFileName);
+std::string &strReplace(std::string &line, std::string &findStr,std::string &replaceWith);
+void replace(const std::string file, std::string findStr, std::string replaceWith);
 
 #endif
--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -3,23 +3,80 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kchan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: kawai <kawai@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 15:58:10 by kchan             #+#    #+#             */
-/*   Updated: 2024/05/09 16:26:21 by kchan            ###   ########.fr       */
+/*   Updated: 2024/05/10 17:53:58 by kawai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Utils.hpp"
 
-void replace(std::string file, std::string findStr, std::string replaceWith)
+std::ifstream readFile(const std::string &file)
 {
-	// std::ifstream	infile;
-	// std::ofstream	outfile;
-	
-	// infile.open(file, std::ios::in);
-	std::cout << "file to replace: " << file << "\nsting to replace: " << findStr << "\nreplace with: " << replaceWith <<std::endl;
+	std::ifstream	inFile;
 
+	inFile.open(file, std::ios::in);
+	if (!inFile.is_open())
+	{
+		std::cout << "Error: failed to open input file" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	return (inFile);
+}
+
+std::ofstream createProcessedFile(std::ifstream &inFile, std::string &outFileName)
+{
+	std::ofstream	outFile;
+		
+	outFile.open(outFileName, std::ios::out);
+	if (!outFile.is_open())
+	{
+		std::cerr << "Error: The output file could not be opened" << std::endl;
+		inFile.close();
+		exit(EXIT_FAILURE);
+	}
+	return (outFile);
+}
+
+std::string &strReplace(std::string &line, std::string &findStr,std::string &replaceWith)
+{
+	std::size_t	index;
+	
+	index = 0;
+	index = line.find(findStr);
+	while(index <= line.length())
+	{
+		line.erase(index, findStr.length());
+		line.insert(index, replaceWith);
+		index = line.find(findStr);
+	}
+	return(line);
+}
+
+// substr (pos) -> means get from "pos" to the end
+void replace(const std::string file, std::string findStr, std::string replaceWith)
+{
+	std::ifstream	inFile;
+	std::ofstream	outFile;
+	std::string		outFileName;
+	std::size_t		lastDotIndex;
+	std::string		line;
+
+	
+	inFile = readFile(file);
+	lastDotIndex = file.find_last_of('.');
+    outFileName = file.substr(0, lastDotIndex) + "_processed" + file.substr(lastDotIndex);
+	outFile = createProcessedFile(inFile, outFileName);
+	while(getline(inFile, line))
+	{
+		strReplace(line, findStr, replaceWith);
+		outFile << line;
+		if(!inFile.eof())
+			outFile << std::endl;
+	}
+	inFile.close();
+	outFile.close();
 }
 
 int main (int argc, char **argv)
@@ -30,7 +87,8 @@ int main (int argc, char **argv)
 	}
 	else
 	{
-		std::cout << "please follow this input pattern: ./replace [infile] [string to find] [replace with]" << std::endl;
+		std::cout << "Usage: ./replace [infile] [string to find] [replace with]" << std::endl;
+		return (EXIT_FAILURE);
 	}
 	return(0);
 }
